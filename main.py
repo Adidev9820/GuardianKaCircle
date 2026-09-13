2d3a8cd1dda529d270df993a0f63bfcb95768646
Adidev <adidevkurumampuzha.com>
2026-09-14 00:08:35 +0530

    Add CORS support

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Guardian Circle Backend")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 latest_event = None
 
